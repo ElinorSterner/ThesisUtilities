4e9008b0ed59ebeeced18b4791b333f3c7846777
ElinorSterner <86856150+ElinorSterner@users.noreply.github.com>
2024-02-16 10:27:24 -0500

    Add files via upload

--- tally_clusters.py
+++ tally_clusters.py
@@ -12,49 +12,54 @@ for clstr_file in os.listdir(input_dir):
 	if clstr_file.endswith("clstr"):
 
 		og = clstr_file.split('.')[0].split("Only_")[1]
-		#print(og)
+		clade_number  = clstr_file.split('.')[2].split("fasta_")[1]
+		clade_data = f'{og}_{clade_number}'
+		#print(clade_data)
 
 		clusters_list = []
 		with open(f'{input_dir}/{clstr_file}', 'r') as clusters:
 			for line in clusters.readlines():
 
-				seq = ''
+				seq = ''#initiate seq list
 				
-				if line[0] == ">":
+				if line[0] == ">":#identify cluster labels with >
 					cluster_name = line.strip('\n')
 				
 				else:
-					seq = line
+					line = line.strip('\n')#remove new line from the end so it can append the og and clade number at the end
+					seq = f'{line}+{clade_data}' # join with a unique connector (+)to make spliting them easy later
 
-				clusters_list.append(f'{cluster_name}, {seq}')
+				clusters_list.append(f'{cluster_name}, {seq}')#save cluster names with list of sequences into a dictionary
 
-			grouped_clust_data = itertools.groupby(clusters_list, lambda x : x[:10]) 
+			grouped_clust_data = itertools.groupby(clusters_list, lambda x : x[:10]) #use groupby() to reformat into items connected by shared cluster number (x[:10]). this is an encoded data type, parsed out in the next few lines
   
 			for cluster_number, sequences in grouped_clust_data: 
-				whole_cluster = {cluster_number : list(sequences)} 
-				all_clades.append(whole_cluster)
+				whole_cluster = {cluster_number : list(sequences)} # make into a dictionary with sequences with the same cluster number together
+				all_clades.append(whole_cluster) # save all clades together, will be easy to parse out next
 
 
-all_clade_data = []
-for clade in all_clades:
+all_clade_data = []#initiate list for data to write out to csv
+
+for clade in all_clades:# for each clstr file in the directory
 	for cluster, items in clade.items():
 
-		cluster_id = cluster
+		cluster_id = cluster[1:10] # get the name of the cluster from the dictionary key 
 
-		jv_count = 0
-		ad_count = 0
-		sz_count = 0
+		#initiate counts for all the cell types for each cluster that we parse through
+		jv_count, ad_count, sz_count = 0, 0, 0
+		multi_count, uni_count, no_data_count = 0, 0, 0
 
-		for item in items:
-			cluster_id = item[1:10]
+		for item in items:#for sequence found in each cluster (this is parsing through a list)
+		#	cluster_id = item[1:10] # cluster name is the first thing in the list I think this line is redundant with the one above
 
+			
+			item_list = item.replace('... ', '_').split(',') # break apart the item (a string), which contains the whole line of the cluster file. it has "..." and spaces so its awkward to work with
 
-			item_list = item.replace('... ', '_').split(',')
-
-			if len(item_list) > 2:
+			if len(item_list) > 2:#if there are sequences in the line (the first line of each list is len = 1 because its ONLY the cluster id)
 				
 				cell = item_list[2][2:12]
 
+				#tally lifestages
 				if 'jv' in cell:
 					jv_count += 1
 				if 'ad' in cell:
@@ -62,7 +67,16 @@ for clade in all_clades:
 				if "sz" in cell:
 					sz_count += 1
 
-				og = item_list[2].split('_')[10]  
+				#tally nuclear types
+				if 'Mu' in cell:
+					multi_count += 1
+				if 'Un' in cell:
+					uni_count += 1
+				if 'Nd' in cell:
+					no_data_count += 1
+
+				#get the og and clade number (from clade grabbing)
+				og = item_list[2].split('+')[-1]
 			
 			else:
 				cell = " "
@@ -72,16 +86,23 @@ for clade in all_clades:
 		porp_of_ad_in_cluster = str(round(ad_count/12, 2))
 		porp_of_sz_in_cluster = str(round(sz_count/9, 2))
 
+		porp_of_mu_in_cluster = str(round(multi_count/8, 2))
+		porp_of_un_in_cluster = str(round(uni_count/6, 2))
+		porp_of_nd_in_cluster = str(round(no_data_count/20, 2))
 
-	to_write_per_cluster = f'{cluster_id}, OG6_{og}, ,{jv_count}, {porp_of_jv_in_cluster}, {ad_count}, {porp_of_ad_in_cluster}, {sz_count}, {porp_of_ad_in_cluster}'
+
+
+
+	to_write_per_cluster = f'{og}, {cluster_id}, , {porp_of_jv_in_cluster},  {porp_of_ad_in_cluster},  {porp_of_sz_in_cluster}, ,{porp_of_mu_in_cluster}, {porp_of_un_in_cluster}, {porp_of_nd_in_cluster}, ,{jv_count},{ad_count},{sz_count}, ,{multi_count}, {uni_count}, {no_data_count}'
 	all_clade_data.append(to_write_per_cluster)
-		#print(cluster_id, og, jv_count, porp_of_jv_in_cluster, ad_count, porp_of_ad_in_cluster)
 
 
-						
-with open("clusters_meta_data.csv", 'w') as o:
+
+with open(f"{input_dir}_meta_data.csv", 'w') as o:
 	
-	header = ("cluster, OG, , jv_count, porp_of_jv_in_cluster, ad_count, porp_of_ad_in_cluster, sz_count, porp_of_ad_in_cluster\n")
+	header = ("OG, cluster, , porp_of_jv_in_cluster,  porp_of_ad_in_cluster,  porp_of_sz_in_cluster, ,porp_of_mu_in_cluster, porp_of_un_in_cluster, porp_of_nd_in_cluster, ,jv_count,ad_count,sz_count, ,multi_count, uni_count, no_data_count\n")
+
+
 	o.write(header)
 
 	for cluster in all_clade_data:
